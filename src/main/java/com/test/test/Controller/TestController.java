c1ad2bb61a16538aad1d46f3470999ead0c48b2d
mahivKhanWasi <wasi@project2morrow.com>
2025-01-14 14:06:01 +0600

    .

--- src/main/java/com/test/test/Controller/TestController.java
+++ src/main/java/com/test/test/Controller/TestController.java
@@ -1,7 +1,10 @@
 package com.test.test.Controller;
 
 
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,5 +19,18 @@ public class TestController {
     @GetMapping("/murgichor")
     public ResponseEntity<?> getMurgiChor() {
         return ResponseEntity.status(HttpStatus.OK).body("Alamin is the murgi chor.");
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Resource> showImage() {
+        try {
+            // Load the image from the static folder
+            Resource image = (Resource) new ClassPathResource("static/images/my-image.jpg");
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "image/jpg")
+                    .body(image);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
